090d0913900d089f81d6f09b9a75c4703d96e9dd
Muhammad Musaib <musaib.999@gmail.com>
2024-12-24 15:06:54 +0530

    Added Key Center

--- screens/Calling.js
+++ screens/Calling.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import {ZegoUIKitPrebuiltCall, ONE_ON_ONE_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn'
+import keyCenter from '../constants';
 export default function VoiceCallPage(props) {
+
     return (
         <View style={styles.container}>
             <ZegoUIKitPrebuiltCall
-                appID={1935548536}
-                appSign={'a092d13fc9a1cc5561b1aeb054551e75f9924eed4aa5322d7eb439a7f1def4f3'}
-                userID={'auxStream'} // userID can be something like a phone number or the user id on your own user system. 
-                userName={'codeWithAuxStream'}
-                callID={'thfytfytfyt'} // callID can be any unique string. 
+                appID={keyCenter.appID}
+                appSign={keyCenter.appSign}
+                userID={props.route.params.data.userID} // userID can be something like a phone number or the user id on your own user system. 
+                userName={props.route.params.data.userName}
+                callID={props.route.params.data.id.toString()} // callID can be any unique string. 
 
                 config={{
                     // You can also use ONE_ON_ONE_VOICE_CALL_CONFIG/GROUP_VIDEO_CALL_CONFIG/GROUP_VOICE_CALL_CONFIG to make more types of calls.
--- screens/Main.js
+++ screens/Main.js
@@ -3,6 +3,19 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList, Image } from 'react-native';
 
 const Main = ({ navigation }) => {
+    const contacts = [  
+        {id: 1, userID: 'auxStream', userName: 'CodeWithAuxStream'},
+        {id: 2, userID: 'jDoe', userName: 'John Doe'},
+        {id: 3, userID: 'james001', userName: 'James Smith'},
+        {id: 4, userID: 'and_rew', userName: 'Andrew Jonas'},
+        {id: 5, userID: 'robb_', userName: 'Robb Stark'},
+        {id: 6, userID: 'musk__', userName: 'Elon Musk'},
+        {id: 7, userID: 'jsnow', userName: 'John Snow'},
+        {id: 8, userID: 'hberg', userName: 'Heisenberg'},
+        {id: 9, userID: 'john', userName: 'Jonathan'},
+    ];
+
+
     return(
        <View style={styles.container}>
             <View style={{ paddingHorizontal: 20, marginTop: 20 }}>
@@ -11,8 +24,8 @@ const Main = ({ navigation }) => {
             
 
             <FlatList
-                data={[1,2,3]}
-                keyExtractor={(item, index) => index}
+                data={contacts}
+                keyExtractor={(item, index) => item.id}
                 style={{ flex: 1, marginTop: 20 }}
                 showsVerticalScrollIndicator={ false }
                 contentContainerStyle={{ paddingHorizontal: 20 }}
@@ -23,29 +36,33 @@ const Main = ({ navigation }) => {
 }
 
 
-const CallingItem = () => {
+const CallingItem = ({item}) => {
     const navigation = useNavigation();
 
     return(
         <View style={styles.callContainer}>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <Image
-                    source={{ uri: 'https://i.pravatar.cc/200?u=auxStream' }}
+                    source={{ uri: `https://i.pravatar.cc/200?u=${item.userName}` }}
                     style={ styles.avatar }
                 />
                 
                 <View style={{ marginLeft: 15 }}>
-                    <Text style={styles.name}>John Doe</Text>
-                    <Text style={styles.username}>@jdoe</Text>
+                    <Text style={styles.name}>{ item.userName }</Text>
+                    <Text style={styles.username}>@{item.userID}</Text>
                     <Text style={styles.time}>1 hour ago</Text>
                 </View>
             </View>
 
             <TouchableOpacity 
-                onPress={() => navigation.navigate('Call')}
+                onPress={() => navigation.navigate('Call', { data: item })}
                 style={styles.callBtn}
             >
-                <Text>C</Text>
+                <Image
+                    source={require('../assets/images/phone.png')}
+                    style={{ width: 20, height: 30 }}
+                    resizeMode='contain'
+                />
             </TouchableOpacity>
         </View>
     )
