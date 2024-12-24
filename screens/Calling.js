776c36301144ce5bca2bdc96a0ce778fa71486bc
Muhammad Musaib <musaib.999@gmail.com>
2024-12-24 17:30:08 +0530

    Some fixes

--- screens/Calling.js
+++ screens/Calling.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import {ZegoUIKitPrebuiltCall, ONE_ON_ONE_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn'
-import keyCenter from '../constants';
+import keyCenter from '../keyStore';
 export default function VoiceCallPage(props) {
 
     return (
@@ -11,7 +11,7 @@ export default function VoiceCallPage(props) {
                 appSign={keyCenter.appSign}
                 userID={props.route.params.data.userID} // userID can be something like a phone number or the user id on your own user system. 
                 userName={props.route.params.data.userName}
-                callID={props.route.params.data.id.toString()} // callID can be any unique string. 
+                callID={'rn12563213515'} // callID can be any unique string. 
 
                 config={{
                     // You can also use ONE_ON_ONE_VOICE_CALL_CONFIG/GROUP_VIDEO_CALL_CONFIG/GROUP_VOICE_CALL_CONFIG to make more types of calls.
